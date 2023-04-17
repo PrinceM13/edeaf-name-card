8e654559c33818fed66e722710842fad0f846203
Parames Pasitprachaja <parames.pasitprachaja@gmail.com>
2023-04-18 00:47:49 +0700

    feat(card): initial ui for name card

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,34 @@
+"use client";
+
+import { useRef } from "react";
+
 export default function Home() {
-  return <div>EDeaf Name Card</div>;
+  return (
+    <>
+      {/* picture */}
+      <div className="p-5 mx-auto mt-20 w-[300px] rounded-xl bg-[#ebf6fc] shadow-gray-800 shadow-lg flex flex-col gap-4">
+        <div className="relative">
+          <img src="/frames/edeaf-frame.png" className="rounded-lg" />
+          <div className="absolute top-[75px] left-[25px] flex flex-col items-center gap-3">
+            <video
+              src="/videos/ink-1.mov"
+              className="h-[200px] w-[200px] rounded-lg"
+              autoPlay
+              loop
+              muted
+            />
+            <div className="text-xl text-[#5384ed] rounded-lg px-2 py-1">Inksy</div>
+          </div>
+        </div>
+
+        {/* contact */}
+        <div>
+          <div>Name : Waruntorn Paonil</div>
+          <div>Email : Ink.Waruntorn@Paonil.com</div>
+          <div>Mobile : 089-999-9999</div>
+          <div>Line Id : Inksy1994</div>
+        </div>
+      </div>
+    </>
+  );
 }
