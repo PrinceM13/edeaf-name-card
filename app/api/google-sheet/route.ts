a22a823df9b505b7f762e5d16f7a5b042e690415
Parames Pasitprachaja <parames.pasitprachaja@gmail.com>
2023-04-19 23:06:44 +0700

    try(fetch): remove request from GET method

--- app/api/google-sheet/route.ts
+++ app/api/google-sheet/route.ts
@@ -1,6 +1,6 @@
 import { getInfos } from "@/utils/google-sheet";
 
-export async function GET(request: Request) {
+export async function GET() {
   const infos = await getInfos();
   console.log("api get all ---> ", infos);
   return new Response(JSON.stringify(infos));
