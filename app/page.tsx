155e876949fc88f9976de056643870b1c68af370
Parames Pasitprachaja <parames.pasitprachaja@gmail.com>
2023-04-19 21:36:17 +0700

    feat(gg-sheet): add button to /name-cards instead of redirect with useEffect

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,26 @@
 "use client";
 
+import CardFrame from "@/components/CardFrame";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function Home() {
   const router = useRouter();
 
-  useEffect(() => {
-    router.push("/name-cards");
-  }, []);
+  // useEffect(() => {
+  //   router.push("/name-cards");
+  // }, []);
 
-  return <></>;
+  return (
+    <>
+      <CardFrame>
+        <button
+          onClick={() => router.push("/name-cards")}
+          className="bg-[#cbe9f6] text-2xl px-2 py-1 rounded-lg text-center"
+        >
+          Go To Name Cards Page
+        </button>
+      </CardFrame>
+    </>
+  );
 }
